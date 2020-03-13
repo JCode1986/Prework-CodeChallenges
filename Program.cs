d0f74dd3bdea60a738713c7aebf4df711d702882
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-03-13 03:27:10 -0700

    method completed

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Prework_CodeChallenges
+{
+    class Program
+    {
+        static int ArrayMaxResult(int[] arr, int num)
+        {
+            int count = 0;
+            foreach(int i in arr)
+            {
+                if(i == num)
+                {
+                    count++;
+                }
+            }
+            return (count > 1) ? count * num : num;
+        }
+
+        static void Main(string[] args)
+        {
+            int [] array = {1, 2, 3, 4, 4, 4, 5};
+            int num = 1;
+            Console.WriteLine(ArrayMaxResult(array, num));
+        }
+    }
+}
