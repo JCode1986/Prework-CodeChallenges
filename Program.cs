14a4e88de44bb2f69d224ded5bd9f0bc20dee247
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-03-13 03:57:08 -0700

    year method completed

--- Program.cs
+++ Program.cs
@@ -9,19 +9,27 @@ namespace Prework_CodeChallenges
             int count = 0;
             foreach(int i in arr)
             {
-                if(i == num)
-                {
-                    count++;
-                }
+                if(i == num) count++;
             }
             return (count > 1) ? count * num : num;
         }
 
+        static string LeapYearCalculator(int year)
+        {
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) return $"{year} is a Leap Year.";
+            else 
+            return $"{year} is not a Leap Year"; 
+        }
         static void Main(string[] args)
         {
+            //Problem #1
             int [] array = {1, 2, 3, 4, 4, 4, 5};
             int num = 1;
             Console.WriteLine(ArrayMaxResult(array, num));
+            
+            //Problem #2
+            int[] years = {1986, 2000, 1453, 1993};
+            Console.WriteLine(LeapYearCalculator(years[0]));
         }
     }
 }
