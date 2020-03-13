9b0fda393db1115246cb99419aef070071fb32ee
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-03-13 16:47:26 -0700

    methods are now public and all strings for easy CLI readability; also spaced out results for each problem

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Prework_CodeChallenges
 {
     class Program
     {
-        static void GetNumbersInArray(int[] arr)
+        public static void GetNumbersInArray(int[] arr)
         {
             foreach (int num in arr) 
             {
@@ -15,7 +15,7 @@ namespace Prework_CodeChallenges
             }
             Console.WriteLine();
         }
-        static int ArrayMaxResult(int[] arr, int num)
+        public static int ArrayMaxResult(int[] arr, int num)
         {
             int count = 0;
             foreach(int i in arr)
@@ -25,14 +25,14 @@ namespace Prework_CodeChallenges
             return (count > 1) ? count * num : num;
         }
 
-        static string LeapYearCalculator(int year)
+        public static string LeapYearCalculator(int year)
         {
             if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) return $"{year} is a Leap Year.";
             else 
             return $"{year} is not a Leap Year"; 
         }
 
-        static void PerfectSequence(int[] arr)
+        public static string PerfectSequence(int[] arr)
         {
             int sum = 0;
             int product = 1;
@@ -41,10 +41,10 @@ namespace Prework_CodeChallenges
                 sum += num;
                 product *= num;
             }
-            Console.WriteLine((sum == product) ? "Yes" : "No"); 
+            return (sum == product) ? "Yes" : "No"; 
         }
 
-        static void SumOfRows(int[,] arr)
+        public static string SumOfRows(int[,] arr)
         {
             int[] result = new int[3];
             int rowSum = 0;
@@ -59,31 +59,37 @@ namespace Prework_CodeChallenges
                     str += $"Row Sum for {i + 1} sub array is {rowSum}\n";
                     rowSum = 0;
                 }
-            Console.WriteLine(str);
+            return str;
         }
         static void Main(string[] args)
         {
             //Problem #1
+            Console.WriteLine("Problem #1");
             int [] array = {1, 2, 3, 4, 4, 4, 5};
-            int num = 1;
-            Console.WriteLine(ArrayMaxResult(array, num));
+            int num = 4;
+            Console.WriteLine($"Result: {ArrayMaxResult(array, num)}");
+            Console.WriteLine();
             
             //Problem #2
+            Console.WriteLine("Problem #2");
             int[] years = {1986, 2000, 1453, 1993};
-            Console.WriteLine(LeapYearCalculator(years[0]));
+            Console.WriteLine($"Result: {LeapYearCalculator(years[0])}");
+            Console.WriteLine();
 
             //Problem #3
+            Console.WriteLine("Problem #3");
             int[] arr1 = {1, 2, 3};
             int[] arr2 = {0, 0, 0};
             int[] arr3 = {1, 2, 3, 4, 5};
-            PerfectSequence(arr1);
+            string answer = PerfectSequence(arr1);
+            Console.WriteLine($"Result: {answer}");
 
             //Problem #4
             int[,] mArray = new int[3, 5]
             { 
                 { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 } 
             };
-            SumOfRows(mArray);
+            Console.WriteLine(SumOfRows(mArray));
         }
     }
 }
