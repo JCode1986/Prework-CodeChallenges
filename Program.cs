6365e4f2aa20efa569a0ad2b51a0ce77101fac37
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-03-13 16:30:41 -0700

    all methods added in branch..

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
 namespace Prework_CodeChallenges
 {
     class Program
     {
+        static void GetNumbersInArray(int[] arr)
+        {
+            foreach (int num in arr) 
+            {
+                Console.Write($"{num}, ");
+            }
+            Console.WriteLine();
+        }
         static int ArrayMaxResult(int[] arr, int num)
         {
             int count = 0;
@@ -20,6 +31,36 @@ namespace Prework_CodeChallenges
             else 
             return $"{year} is not a Leap Year"; 
         }
+
+        static void PerfectSequence(int[] arr)
+        {
+            int sum = 0;
+            int product = 1;
+            foreach(int num in arr)
+            {
+                sum += num;
+                product *= num;
+            }
+            Console.WriteLine((sum == product) ? "Yes" : "No"); 
+        }
+
+        static void SumOfRows(int[,] arr)
+        {
+            int[] result = new int[3];
+            int rowSum = 0;
+            string str = "\nProblem #4\n";
+            for (int i = 0; i < 3; i++)
+                {
+                for (int j = 0; j < 5; j++)
+                {
+                    rowSum += arr[i, j];
+                }
+                    result[i] = rowSum;
+                    str += $"Row Sum for {i + 1} sub array is {rowSum}\n";
+                    rowSum = 0;
+                }
+            Console.WriteLine(str);
+        }
         static void Main(string[] args)
         {
             //Problem #1
@@ -30,6 +71,19 @@ namespace Prework_CodeChallenges
             //Problem #2
             int[] years = {1986, 2000, 1453, 1993};
             Console.WriteLine(LeapYearCalculator(years[0]));
+
+            //Problem #3
+            int[] arr1 = {1, 2, 3};
+            int[] arr2 = {0, 0, 0};
+            int[] arr3 = {1, 2, 3, 4, 5};
+            PerfectSequence(arr1);
+
+            //Problem #4
+            int[,] mArray = new int[3, 5]
+            { 
+                { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 } 
+            };
+            SumOfRows(mArray);
         }
     }
 }
