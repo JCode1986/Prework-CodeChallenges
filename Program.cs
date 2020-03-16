1183e14ddbaf9ad6378fbcf94bb817a5ca546f26
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-03-15 22:54:18 -0700

    problem 1 now takes a user input

--- Program.cs
+++ Program.cs
@@ -7,22 +7,50 @@ namespace Prework_CodeChallenges
 {
     class Program
     {
-        public static void GetNumbersInArray(int[] arr)
+        public static string GetNumbersInArray(int[] arr)
         {
+            string str = "Array: ";
             foreach (int num in arr) 
             {
-                Console.Write($"{num}, ");
+                str += Convert.ToString(num);
+                str += ", ";
             }
-            Console.WriteLine();
+            return str;
         }
-        public static int ArrayMaxResult(int[] arr, int num)
+        
+        // public static int ArrayMaxResult(int[] arr, int num)
+        // {
+        //     int count = 0;
+        //     foreach(int i in arr)
+        //     {
+        //         if(i == num) count++;
+        //     }
+        //     return (count > 1) ? count * num : num;
+        // }
+        public static string ArrayMaxResult()
         {
-            int count = 0;
-            foreach(int i in arr)
+            Console.WriteLine("Enter size of array: ");
+            int sizeInput = int.Parse(Console.ReadLine());
+            int[] arrayInput = new int[sizeInput]; 
+
+            for(int i = 0; i < arrayInput.Length; i++)
             {
-                if(i == num) count++;
+                Console.WriteLine($"{i + 1}: Enter element");
+                arrayInput[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine(GetNumbersInArray(arrayInput));
             }
-            return (count > 1) ? count * num : num;
+
+            GetNumbersInArray(arrayInput);
+            Console.WriteLine("Enter an Integer");
+            int target = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            foreach(int i in arrayInput)
+            {
+                if(i == target) count++;
+            }
+            int check = (count > 1) ? count * target : target;
+            return $"Result: {check}";
         }
 
         public static string LeapYearCalculator(int year)
@@ -59,47 +87,42 @@ namespace Prework_CodeChallenges
         }
         static void Main(string[] args)
         {
-
             //Problem #1
             Console.WriteLine("************Problem #1 Array Max Result************");
-            int [] array = {1, 2, 3, 4, 4, 4, 5};
-            int num = 4;
-            Console.WriteLine($"Result with duplicate target number: {ArrayMaxResult(array, num)}");
-            Console.WriteLine($"Result with only unique numbers: {ArrayMaxResult(array, 2)}");
-            Console.WriteLine();
+            Console.WriteLine(ArrayMaxResult());
             
             //Problem #2
-            Console.WriteLine("************Problem #2 Leap Year Calculator************");
-            int[] years = {1986, 2000, 1453, 1993};
-            Console.WriteLine($"Result Leap Year: {LeapYearCalculator(years[0])}");
-            Console.WriteLine($"Result Not Leap Year: {LeapYearCalculator(years[1])}");
-            Console.WriteLine();
+        //     Console.WriteLine("************Problem #2 Leap Year Calculator************");
+        //     int[] years = {1986, 2000, 1453, 1993};
+        //     Console.WriteLine($"Result Leap Year: {LeapYearCalculator(years[0])}");
+        //     Console.WriteLine($"Result Not Leap Year: {LeapYearCalculator(years[1])}");
+        //     Console.WriteLine();
 
-            //Problem #3
-            Console.WriteLine("************Problem #3 Perfect Sequence************");
-            int[] arr1 = {1, 2, 3};
-            int[] arr2 = {0, 0, 0};
-            int[] arr3 = {1, 2, 3, 4, 5};
-            string answerOne = PerfectSequence(arr1);
-            string answerTwo = PerfectSequence(arr3);
-            Console.WriteLine($"Result Perfect Sequence: {answerOne}");
-            Console.WriteLine($"Result Not Perfect Sequence: {answerTwo}");
-            Console.WriteLine();
+        //     //Problem #3
+        //     Console.WriteLine("************Problem #3 Perfect Sequence************");
+        //     int[] arr1 = {1, 2, 3};
+        //     int[] arr2 = {0, 0, 0};
+        //     int[] arr3 = {1, 2, 3, 4, 5};
+        //     string answerOne = PerfectSequence(arr1);
+        //     string answerTwo = PerfectSequence(arr3);
+        //     Console.WriteLine($"Result Perfect Sequence: {answerOne}");
+        //     Console.WriteLine($"Result Not Perfect Sequence: {answerTwo}");
+        //     Console.WriteLine();
 
-            //Problem #4
-            int[,] mArrayOne = new int[3, 5]
-            { 
-                { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 } 
-            };
-            int[,] mArrayTwo = new int[7, 8]
-            { 
-                { 1, 2, 3, 4, 5, 6, 7, 8 }, { 6, 7, 8, 9, 10, 11, 12, 13 }, { 11, 12, 13, 14, 15, 16,17, 20 }, 
-                { 1, 2, 3, 4, 5, 6, 7, 23 }, { 6, 7, 8, 9, 10, 11, 12, 40 }, { 11, 12, 13, 14, 15, 16,17, 69 }, 
-                { 1, 2, 3, 4, 5, 6, 10, 420 } 
-            };
-            Console.WriteLine("************Problem #4 Sum of Rows************");
-            Console.WriteLine($"Result 1 with {mArrayOne.GetLength(0)} Rows:\n{SumOfRows(mArrayOne)}");
-            Console.WriteLine($"Result 2 with {mArrayTwo.GetLength(0)} Rows:\n{SumOfRows(mArrayTwo)}");
+        //     //Problem #4
+        //     int[,] mArrayOne = new int[3, 5]
+        //     { 
+        //         { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 } 
+        //     };
+        //     int[,] mArrayTwo = new int[7, 8]
+        //     { 
+        //         { 1, 2, 3, 4, 5, 6, 7, 8 }, { 6, 7, 8, 9, 10, 11, 12, 13 }, { 11, 12, 13, 14, 15, 16,17, 20 }, 
+        //         { 1, 2, 3, 4, 5, 6, 7, 23 }, { 6, 7, 8, 9, 10, 11, 12, 40 }, { 11, 12, 13, 14, 15, 16,17, 69 }, 
+        //         { 1, 2, 3, 4, 5, 6, 10, 420 } 
+        //     };
+        //     Console.WriteLine("************Problem #4 Sum of Rows************");
+        //     Console.WriteLine($"Result 1 with {mArrayOne.GetLength(0)} Rows:\n{SumOfRows(mArrayOne)}");
+        //     Console.WriteLine($"Result 2 with {mArrayTwo.GetLength(0)} Rows:\n{SumOfRows(mArrayTwo)}");
         }
     }
 }
